a00b43e0ec9f484990efb782c2f857002e2bec2b
Anum Mujahid <amujahid.bese18seecs@student.nust.edu.pk>
2021-01-27 14:47:54 +0500

    Adding partials

---
+++ partials/header.php
@@ -0,0 +1,31 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link rel="shortcut icon" href="#">
+    <title>Notesy</title>
+    <style>
+    a{
+        text-decoration: none;
+        color: white;
+    }
+    a:hover{
+        text-decoration: none;
+        color: white;
+    }
+    </style>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container px-0">
+        <a class="navbar-brand" href="#">Notesy</a>
+    </div>
+    </nav>
